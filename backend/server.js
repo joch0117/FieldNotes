d3f4df52a21bf9fe3c6503f3bcb490ab94f243b3
joch 0117 <jochaigneau1@gmail.com>
2026-05-01 11:08:54 +0200

    database install conexion ok , data test ok

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,18 @@ app.get('/api/health', (req,res)=>{
     res.json({ message:'API FieldNotes OK' })
 })
 
+const db = require('./src/database/db')
+
+app.get('/api/db-test', async (req, res) => {
+  try {
+    const [rows] = await db.execute('SELECT 1 + 1 AS result')
+    res.json(rows[0])
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Erreur connexion BDD' })
+  }
+})
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, '0.0.0.0', ()=>{
